09506fa2d052a3d925218e691126fb7f308955b3
MSLadanov <MSLadanov@gmail.com>
2025-06-02 18:20:59 +0300

    chore: :sparkles: creating useTodos hook

---
+++ src/hooks/useTodos.ts
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+export interface ITodo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+export const useTodos = () => {
+  const [todos, setTodos] = useState<ITodo[]>([]);
+  const [inputValue, setInputValue] = useState("");
+  const addTodo = () => {
+    if (inputValue.trim()) {
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          text: inputValue,
+          completed: false,
+        },
+      ]);
+      setInputValue("");
+    }
+  };
+  const toggleTodo = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+  const deleteTodo = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+  const clearCompleted = () => {
+    setTodos(todos.filter((todo) => !todo.completed));
+  };
+  const pendingCount = todos.filter((todo) => !todo.completed).length;
+  const completedCount = todos.filter((todo) => todo.completed).length;
+  return {
+    todos,
+    inputValue,
+    setInputValue,
+    addTodo,
+    toggleTodo,
+    deleteTodo,
+    clearCompleted,
+    pendingCount,
+    completedCount,
+  };
+};
